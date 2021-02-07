169258183cb5c1e80a402c9dcbc5bd8a3f483497
NtwaliHeritier <heritierntwali@gmail.com>
2021-02-07 03:54:35 +0200

    add state to the app.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,23 @@ import ButtonPanel from './buttonPanel';
 import Display from './display';
 import calculate from '../logic/calculate';
 
-const App = () => {
-  return (
-    <>
+class App extends Component {
+  constructor() {
+    super();
+    state = { 
+      total: null,
+      next: null,
+      operation: null
+     };
+  }
+  render() { 
+    return ( 
+      <>
       <Display result="0" />
       <ButtonPanel />
     </>
-  );
+     );
+  }
 }
-
+ 
 export default App;
