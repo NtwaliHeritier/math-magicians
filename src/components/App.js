e9e5d6f01940afa70bc7ff5e050667e8f2b5c389
NtwaliHeritier <heritierntwali@gmail.com>
2021-02-09 13:46:41 +0200

    fix multiple operations issue

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     super();
     this.state = {
       total: '0',
-      next: '',
+      next: '0',
       operation: '',
     };
   }
--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -4,18 +4,18 @@ const calculate = (data, buttonName) => {
   let { total, next, operation } = data;
   if (buttonName === 'AC') {
     total = '0';
-    next = '';
+    next = '0';
     operation = '';
   } else if (buttonName === '.') {
     if (/\./.test(total) === false) total += '.';
   } else if (/\d/.test(buttonName)) {
-    if ((operation === '' && next === '') || operation !== '') {
+    if ((operation === '' && next === '0') || operation !== '') {
       if (total === '0') {
         total = buttonName;
       } else {
         total += buttonName;
       }
-    } else if (next === '') {
+    } else if (next === '0') {
       next = total;
       total = buttonName;
     } else {
@@ -27,10 +27,15 @@ const calculate = (data, buttonName) => {
     total = operate(total, '100', '÷');
   } else if (buttonName !== '=') {
     operation = buttonName;
-    next = total;
+    if (next === '0') {
+      next = total;
+    } else {
+      next = operate(next, total, operation);
+    }
     total = '0';
   } else {
     total = operate(next, total, operation);
+    next = '0';
   }
   return { total, next, operation };
 };
