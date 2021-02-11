948e4924d21cab112812f5b2f4420304251ac74b
NtwaliHeritier <heritierntwali@gmail.com>
2021-02-11 03:43:25 +0200

    add style to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,23 @@
 import {Link} from 'react-router-dom';
 
 const Navbar = () => {
+  const styles = {
+    display: 'flex',
+    justifyContent: 'space-between'
+  };
+
+  const links = {
+    width: '25%',
+    display: 'flex',
+    justifyContent: 'space-evenly'
+  };
+  
   return ( 
-    <div>
+    <div style = {styles}>
       <div>Math Magicians</div>
-      <div>
-        <Link to = "/">Home</Link>
-        <Link to = "/calculator">calculator</Link>
+      <div style = {links}>
+        <Link to = "/">Home</Link> |
+        <Link to = "/calculator">calculator</Link> |
         <Link to = "/quote">Quote</Link>
       </div>
     </div>
--- src/components/quote.js
+++ src/components/quote.js
@@ -1,6 +1,9 @@
 const Quote = () => {
   return ( 
-    <div>Quote</div>
+    <p>
+      Mathematics is not about numbers, equations, computations,
+      or algorithms: it is about understanding. –William Paul Thurston
+    </p>
    );
 }
  
