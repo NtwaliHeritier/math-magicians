e1696158b898b530afb4d8b80526f9ca3c9d6ba3
NtwaliHeritier <heritierntwali@gmail.com>
2021-02-17 15:41:01 +0200

    add negative tests

--- src/__tests__/operate-test.js
+++ src/__tests__/operate-test.js
@@ -8,12 +8,24 @@ describe('Operator', () => {
     expect(operate(a, b, '+')).toEqual('3');
   });
 
+  it('returns the sum', () => {
+    expect(operate(a, b, '+')).not.toEqual('4');
+  });
+
   it('returns the difference', () => {
     expect(operate(a, b, '-')).toEqual('-1');
   });
 
+  it('returns the difference', () => {
+    expect(operate(a, b, '-')).not.toEqual('1');
+  });
+
   it('returns the product', () => {
     expect(operate(a, b, 'x')).toEqual('2');
+  });
+
+  it('returns the product', () => {
+    expect(operate(a, b, 'x')).not.toEqual('3');
   });
 
   it('returns the quotient', () => {
@@ -21,6 +33,14 @@ describe('Operator', () => {
   });
 
   it('returns the quotient', () => {
+    expect(operate(a, b, '÷')).not.toEqual('1');
+  });
+
+  it('returns the quotient', () => {
     expect(operate(a, 0, '÷')).toEqual("Can't be divided by 0");
+  });
+
+  it('returns the quotient', () => {
+    expect(operate(a, 0, '÷')).not.toEqual("0");
   });
 });
