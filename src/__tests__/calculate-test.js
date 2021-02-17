0be8e07f515783e779da7240cc228a9ffce0312f
NtwaliHeritier <heritierntwali@gmail.com>
2021-02-17 14:14:58 +0200

    add test cases for calculate and operate

--- src/__tests__/calculate-test.js
+++ src/__tests__/calculate-test.js
@@ -1,13 +1,33 @@
-import calculate from '../logic/calculate'
+import calculate from '../logic/calculate';
 
-let data = {total: '0', next: '0', operation: ''}
+const data = { total: '1', next: '0', operation: '' };
 
-describe("Calculator", ()=>{
-  it("tests AC",()=>{
-    expect(calculate(data, "AC")).toEqual({total: '0', next: '0', operation: ''});
-  })
+describe('Calculator', () => {
+  it('tests AC', () => {
+    expect(calculate(data, 'AC')).toEqual({ total: '0', next: '0', operation: '' });
+  });
 
-  it("tests .", ()=>{
-    expect(calculate(data, ".")).toEqual({total: '0.', next: '0', operation: ''})
-  })
-})
+  it('tests .', () => {
+    expect(calculate(data, '.')).toEqual({ total: '1.', next: '0', operation: '' });
+  });
+
+  it('tests +/-', () => {
+    expect(calculate(data, '+/-')).toEqual({ total: '-1', next: '0', operation: '' });
+  });
+
+  it('tests %', () => {
+    expect(calculate(data, '%')).toEqual({ total: '0.01', next: '0', operation: '' });
+  });
+
+  it('tests +', () => {
+    expect(calculate({ total: '1', next: '2', operation: '+' }, '=')).toEqual({ total: '3', next: '0', operation: '+' });
+  });
+
+  it('tests -', () => {
+    expect(calculate({ total: '1', next: '2', operation: '-' }, '=')).toEqual({ total: '1', next: '0', operation: '-' });
+  });
+
+  it('tests x', () => {
+    expect(calculate({ total: '1', next: '2', operation: 'x' }, '=')).toEqual({ total: '2', next: '0', operation: 'x' });
+  });
+});
